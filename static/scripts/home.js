fcb0224bcf5e83a5d9917dad8f61a00b02a3efb1
Debbie Yuen <debbieyuen@Debbies-iMac.local>
2020-12-04 17:33:56 -0800

    edits

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -6,6 +6,6 @@ jQuery(document).ready(function(){
     $('h1').mousemove(function(e){
       var rXP = (e.pageX - this.offsetLeft-$(this).width()/2);
       var rYP = (e.pageY - this.offsetTop-$(this).height()/2);
-      $('h1').css('text-shadow', +rYP/10+'px '+rXP/80+'px rgba(255,0,255,.8), '+rYP/8+'px '+rXP/60+'px rgba(255,237,0,1), '+rXP/70+'px '+rYP/12+'px rgba(0,255,255,.7)');
+      $('h1').css('text-shadow', +rYP/150+'px '+rXP/100+'px rgba(255,0,255,.8), '+rYP/150+'px '+rXP/70+'px rgba(255,237,0,1), '+rXP/100+'px '+rYP/150+'px rgba(0,255,255,.7)');
     });
  });
