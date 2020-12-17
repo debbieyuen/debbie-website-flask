5612b8aa04e5bd65ee0829a3e63ff119c3592160
Debbie Yuen <debbieyuen@Debbies-iMac.local>
2020-12-17 15:21:08 -0800

    Fix

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -4,9 +4,6 @@
 
 // gsap.registerPlugin(DrawSVGPlugin);
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
 $(function(){
     var $container = $('#Portfolio');
   
@@ -50,32 +47,3 @@ $(function(){
 
     gsap.to(".sidebar", {duration: 1, x: 100});
 
-
-
-
-    
-    // Next/previous controls
-    function plusSlides(n) {
-      showSlides(slideIndex += n);
-    }
-    
-    // Thumbnail image controls
-    function currentSlide(n) {
-      showSlides(slideIndex = n);
-    }
-    
-    function showSlides(n) {
-      var i;
-      var slides = document.getElementsByClassName("mySlides");
-      var dots = document.getElementsByClassName("dot");
-      if (n > slides.length) {slideIndex = 1}
-      if (n < 1) {slideIndex = slides.length}
-      for (i = 0; i < slides.length; i++) {
-          slides[i].style.display = "none";
-      }
-      for (i = 0; i < dots.length; i++) {
-          dots[i].className = dots[i].className.replace(" active", "");
-      }
-      slides[slideIndex-1].style.display = "block";
-      dots[slideIndex-1].className += " active";
-    }
